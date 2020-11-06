4569dae6b359195c332c4e5b7b7577ba4301e205
sivaprasadsa <53429732+sivaprasadsa@users.noreply.github.com>
2020-11-06 15:25:51 +0530

    Add files via upload
    before erro

--- App.py
+++ App.py
@@ -11,13 +11,13 @@ app.secret_key = "Secret Key"
 #SqlAlchemy Database Configuration With Mysql
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/crud'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config["WHOOSH_BASE"]='whoosh'
+
 db = SQLAlchemy(app)
 
 
 #Creating model table for our CRUD database
 class Data(db.Model):
-    __searchable__=['name','lastname', 'home', 'email'] 
+    
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100))
     lastname =db.Column(db.String(100))
@@ -26,7 +26,8 @@ class Data(db.Model):
     phone = db.Column(db.String(100))
     createdOn = db.Column(db.DateTime)
     updatedOn = db.Column(db.DateTime)
-    def __init__(self, name, lastname, home, email, phone,created,updatedOn):
+    placeid = db.Column(db.Integer)
+    def __init__(self, name, lastname, home, email, phone,created,updatedOn,placeid):
 
         self.name = name
         self.lastname = lastname
@@ -34,7 +35,14 @@ class Data(db.Model):
         self.email = email
         self.phone = phone
         self.createdOn=created
+        self.placeid=placeid
+class Places(db.Model):
+    
+    id = db.Column(db.Integer, primary_key = True)
+    placename = db.Column(db.String(100))
 
+def place_query():
+    return Places.query       
 
 
 
@@ -44,17 +52,25 @@ class Data(db.Model):
 @app.route('/')
 def Index():
     all_data = Data.query.all()
+    place= Places.query.all()
+    data =dict()
+    data['employees'] = all_data
+    data['places']= place
+    return render_template("index.html", data = data)
 
-    return render_template("index.html", employees = all_data)
-
-
-
+@app.route('/place')
+def Place():
+    all_places = Places.query.all()
+    data= all_places
+    #return render_template("places.html",placelist=all_places)
+    return render_template("places.html",data=data)
 #this route is for inserting data to mysql database via html forms
 @app.route('/insert', methods = ['POST'])
+
+
 def insert():
-
+    
     if request.method == 'POST':
-
         name = request.form['name']
         lastname= request.form['lastname']
         home = request.form['home']
@@ -63,12 +79,28 @@ def insert():
         today = date.today()
         created =today
         updated =''
-
-        my_data = Data(name, lastname, home, email, phone, created,updated)
+        
+        placename= Places.query.get(home)
+        my_data = Data(name, lastname, placename.placename, email, phone, created,updated,placename.id)
         db.session.add(my_data)
         db.session.commit()
-
         flash("Employee Inserted Successfully")
+        #return render_template('index.html')
+        return redirect('/')
+#add places
+@app.route('/insertplace', methods = ['POST'])
+def insertplace():
+
+    if request.method == 'POST':
+        #import pdb;pdb.set_trace()
+        placename = request.form['placename']
+        
+        my_place = Places()
+        my_place.placename=placename
+        db.session.add(my_place)
+        db.session.commit()
+        
+        flash("Place Inserted Successfully")
 
         return redirect(url_for('Index'))
 
@@ -108,14 +140,28 @@ def delete(id):
 
     return redirect(url_for('Index'))
 
+@app.route('/deleteplace/<id>/', methods = ['GET', 'POST'])
+def deleteplace(id):
+    my_data = Places.query.get(id)
+    db.session.delete(my_data)
+    db.session.commit()
+    flash("Place Deleted Successfully")
+
+    return redirect(url_for('Index'))
+
 
 @app.route('/search',methods=['GET','POST'])
 def search():
+
     form=request.form
     search_key =form['key']
     my_Data="%{0}%".format(search_key)
     my_data = Data.query.filter(Data.name.like(my_Data)).all()
-    return render_template("index.html", employees = my_data)
+    place= Places.query.all()
+    data =dict()
+    data['employees'] = my_data
+    data['places']= place
+    return render_template("index.html", data = data)
  
 
 if __name__ == "__main__":
